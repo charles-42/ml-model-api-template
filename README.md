c99ced48678d81f2fb4ae933ec162c4bb5a50074
charles-42 <71760090+charles-42@users.noreply.github.com>
2024-04-26 15:14:47 +0200

    docs: :art: use poetry

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ sqlite3 olist.db < database_building/create_table.sql
 sqlite3 olist.db < database_building/import_table.sql 2>/dev/null
 ```
 
-3. Creatre environment variables;
+3. Create environment variables;
 
 In the api folder, create a .env file with a SECRET_KEY variable
 
--- model/train.sh
+++ model/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define your Python script and its parameters
-run_name="remote_run"
+run_name="poetry_run"
 start_date="2017-01-01"
 end_date="2018-01-01"
 
 # Run the Python script with parameters
-# python -m model.main $run_name $start_date $end_date 
+python -m model.main $run_name $start_date $end_date 
 
 # # # Define your pytest command and its parameters
 run_name_pytest="--run_name=$run_name"
