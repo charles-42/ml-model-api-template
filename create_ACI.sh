275c156f859f69a289585654ff96bd6007b1058d
charles-42 <71760090+charles-42@users.noreply.github.com>
2024-05-09 14:29:38 +0200

    cd fix

---
+++ create_ACI.sh
@@ -0,0 +1,22 @@
+set -o allexport
+source .env
+set +o allexport
+
+az container create \
+            --resource-group $RESSOURCE_GROUP \
+            --name apimodeletemplate \
+            --image $DOCKERHUB_USERNAME/api_modele:latest \
+            --cpu 1 \
+            --memory 1 \
+            --ip-address public \
+            --ports 80
+            --port 8000:8000
+            --environment-variables \
+              "SUBSCRIPTION_ID=$SUBSCRIPTION_ID \
+              "RESSOURCE_GROUP"=$RESSOURCE_GROUP \
+              "WORKSPACE_NAME"=$WORKSPACE_NAME \
+              "SERVER=$SERVER \
+              "DATABASE=$DATABASE \
+              "POSTGRES_USER=$POSTGRES_USER \
+              "PASSWORD=$PASSWORD \
+              "SECRET_KEY=$SECRET_KEY
