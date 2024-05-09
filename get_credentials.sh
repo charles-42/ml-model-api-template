03e4ca4e0b7fad40716594764fd50f7cbf5867b2
charles-42 <71760090+charles-42@users.noreply.github.com>
2024-05-09 14:03:14 +0200

    ci(ci): :green_heart: try ci.cd pipeline

--- get_credentials.sh
+++ get_credentials.sh
@@ -2,11 +2,28 @@ set -o allexport
 source .env
 set +o allexport
 
+# This is suppose to be the way to create app credentials
+# az account set --subscription $SUBSCRIPTION_ID
+
+# az ad sp create-for-rbac \
+#     --name myApp \
+#     --role contributor \
+#     --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESSOURCE_GROUP \
+#     --json-auth
+
+
+# Create the application and capture the output in JSON format
+APP_JSON=$(az ad app create --display-name "Github_Login")
+
+# Extract the appId using jq and store it in a variable
+APP_ID=$(echo $APP_JSON | jq -r '.appId')
+
+az ad app update --id $APP_ID  --set groupMembershipClaims=All
+
+az ad sp create --id $APP_ID
+
 az account set --subscription $SUBSCRIPTION_ID
 
-az ad sp create-for-rbac \
-    --name myApp \
-    --role contributor \
-    --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESSOURCE_GROUP \
-    --json-auth
-       
+az role assignment create --assignee $APP_ID --role Contributor --scope subscriptions/$SUBSCRIPTION_ID
+
+az ad sp credential reset --id $APP_ID --append --years 2
