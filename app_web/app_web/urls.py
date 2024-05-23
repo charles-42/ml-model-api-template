042e103e42a98c0ecc31bdfca420102469aa2e61
charles-42 <71760090+charles-42@users.noreply.github.com>
2024-05-23 15:13:42 +0200

    try ci app web

--- app_web/app_web/urls.py
+++ app_web/app_web/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('query/', include('query_app.urls')),
+    path('', include('query_app.urls')),
 ]
